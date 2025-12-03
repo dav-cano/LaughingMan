e075993bb56130ab188d66ccc6a61ce1f8f87ba6
dav-cano <davecano.244@gmail.com>
2025-12-02 20:20:58 -0500

    Bullets 5/6. Mouseovers for orochimaru and #square

--- laughingman.js
+++ laughingman.js
@@ -1,30 +1,68 @@
-var SQUARE_COUNT = 3;
-var SPEED = 5;
-var TIMER_SPEED = 100;
+const SQUARE_COUNT = 3;
+const SPEED = 5;
+const TIMER_SPEED = 100;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', ()=>{
     let squareElement = document.getElementById("square");
-    //let squareElement = document.querySelector("#square");
-    squareElement.addEventListener("click", () => {
+    squareElement.addEventListener("click", ()=>{
         alert("OMG YOU CAUGHT ME!");
     });
 
     let box = document.querySelector("#box");
 
-    for(let i = 0; i < SQUARE_COUNT; i++) {
-        //create the square
+    for(let i = 0;i<SQUARE_COUNT;i++)
+    {
         let square = document.createElement('img');
         square.src = 'laughing_man.jpg';
         square.alt = 'Catch the Laughing Man!';
         square.className = 'square';
         box.appendChild(square);
-        //add it to the screen
     }
     let allSquares = box.children;
-    let squareArray = Array.from(children);
-    squareArray.forEach(function(element, index){
-        var dx = SPEED * (Math.random()*2 -1);
-        var dy = SPEED * (Math.random()*2 -1);
+    let squareArray = Array.from(allSquares);
+    squareArray.forEach(function (element, index) {
+        var dx = SPEED * Math.random() * 2 - 1;
+        var dy = SPEED * Math.random() * 2 - 1;
+        setInterval(() => {
+            let currentLeft = parseInt(element.style.left) || 225;
+            let currentTop = parseInt(element.style.top) || 175;
+            if (currentLeft >= 450 || currentLeft <= 0) {
+                dx *= -1;
+                element.style.borderColor = getRandomColor();
+            }
+            if (currentTop >= 350 || currentTop <= 0) {
+                dy *= -1;
+                element.style.borderColor = getRandomColor();
+            }
+            element.style.left = (currentLeft + dx) + "px";
+            element.style.top = (currentTop + dy) + "px";
+        }, TIMER_SPEED);
+
+        
+        let currentImage = element.src;
+        element.addEventListener("mouseover", ()=>{
+            element.src = "orochimaru.jpg";
+        });
+        element.addEventListener("mouseout", ()=>{
+            element.src = currentImage;
     });
-    
-});
+
+        let currentSquare = document.getElementById("square");
+
+        currentSquare.addEventListener("mouseover", ()=>{
+            currentSquare.style.backgroundColor = getRandomColor();
+        });
+
+        currentSquare.addEventListener("mouseout", ()=>{
+            currentSquare.style.backgroundColor = "red";
+        });
+    });             
+
+    function getRandomColor() {
+        let r = parseInt(Math.random() * 256);
+        let g = parseInt(Math.random() * 256);
+        let b = parseInt(Math.random() * 256);
+        let color = `rgb(${r}, ${g}, ${b})`;
+        return color;
+    }
+    });
